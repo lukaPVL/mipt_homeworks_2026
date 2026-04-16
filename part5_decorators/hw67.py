a85c36b704263da888e19520efcd59d43363fcac
Luka Pavlov <pavlovld2007@gmail.com>
2026-04-16 15:14:01 +0300

    add homework: decorator and custom error handling

--- part5_decorators/hw67.py
+++ part5_decorators/hw67.py
@@ -1,3 +1,6 @@
+import time
+from datetime import datetime, timezone
+import functools
 import json
 from typing import Any, ParamSpec, Protocol, TypeVar
 from urllib.request import urlopen
@@ -20,19 +23,74 @@ class CallableWithMeta(Protocol[P, R_co]):
 
 
 class BreakerError(Exception):
-    pass
+    def __init__(self, message: str, func_name: str, block_time: datetime):
+        super().__init__(message)
+        self.func_name = func_name
+        self.block_time = block_time
 
 
 class CircuitBreaker:
     def __init__(
         self,
-        critical_count: int,
-        time_to_recover: int,
-        triggers_on: type[Exception],
-    ): ...
+        critical_count: int = 5,
+        time_to_recover: int = 30,
+        triggers_on: type[Exception] = Exception,
+    ):
+        errors = []
+        if not isinstance(critical_count, int) or critical_count <= 0:
+            errors.append(ValueError(INVALID_CRITICAL_COUNT))
+        
+        if not isinstance(time_to_recover, int) or time_to_recover <= 0:
+            errors.append(ValueError(INVALID_RECOVERY_TIME))
+
+        if errors:
+            raise ExceptionGroup(VALIDATIONS_FAILED, errors)
+        
+        self.critical_count = critical_count
+        self.time_to_recover = time_to_recover
+        self.triggers_on = triggers_on
+
+        self._fail_count = 0
+        self._last_fail_time = None
+
+
+    def _check_state(self, func: CallableWithMeta) -> None:
+        if self._last_fail_time is not None:
+            if time.time() - self._last_fail_time < self.time_to_recover:
+                block_date_time = datetime.fromtimestamp(self._last_fail_time, tz=timezone.utc)
+                raise BreakerError(
+                    TOO_MUCH,
+                    f"{func.__module__}.{func.__name__}",
+                    block_date_time
+                )
+            self._last_fail_time = None
+
+
+    def _handle_failure(self, func: CallableWithMeta, error: Exception) -> None:
+        self._fail_count += 1
+        if self._fail_count >= self.critical_count:
+            self._last_fail_time = time.time()
+            block_date_time = datetime.fromtimestamp(self._last_fail_time, tz=timezone.utc)
+            raise BreakerError(
+                TOO_MUCH,
+                f"{func.__module__}.{func.__name__}",
+                block_date_time
+            ) from error
+        raise error
+
 
     def __call__(self, func: CallableWithMeta[P, R_co]) -> CallableWithMeta[P, R_co]:
-        raise NotImplementedError
+        @functools.wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R_co:
+            self._check_state(func)
+            try:
+                result = func(*args, **kwargs)
+                self._fail_count = 0
+                return result
+            except self.triggers_on as e:
+                self._handle_failure(func, e)
+
+        return wrapper
 
 
 circuit_breaker = CircuitBreaker(5, 30, Exception)
